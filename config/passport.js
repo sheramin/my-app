6a56f3bbd5aaa5e4510a1ff205d18a1f7cb09c3d
Amin Sherzad <aminsherzad90@gmail.com>
2017-10-24 13:38:06 +0430

    first commit

---
+++ config/passport.js
@@ -0,0 +1,24 @@
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const User = require('../models/users');
+const config = require('../config/database');
+
+module.exports = function(passport){
+    let opts = {};
+    // opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+    opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme('jwt');
+    opts.secretOrKey = config.secret;
+    passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
+        console.log('Inside passport.use');
+        User.getUserById(jwt_payload.data._id, (err, user) => {
+            if(err){
+                return done(err, false);
+            }
+            if(user){
+                return done(null, user);
+            }else{
+                return done(null, false);
+            }
+        });
+    }));
+}
